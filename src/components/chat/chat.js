53551d008dfa593ecb37521755d38186f81a39b1
Sue <sue.li@post.harvard.edu>
2019-03-18 11:36:28 +0100

    fix initial file structure

---
+++ src/components/chat/chat.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './chat.css';
+
+class Chat extends React.Component {
+
+  render() {
+    return(
+      <div>
+      </div>
+    )
+  }
+}
+
+
+export default Chat;
