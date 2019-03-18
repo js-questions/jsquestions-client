a4ceea425da0fb4d5bfadfeea894fe71d5cff9bf
Amber Williams <Amber-Williams@users.noreply.github.com>
2019-03-18 18:56:49 +0100

    Feat: React router set up, Feature: Ask question page built out and awaiting other build outs to complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
+
+import AskQuestions from './components/ask-questions-page/ask-questions';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Router>
+        <div>
+          <Route path="/ask" component={AskQuestions}/>
+        </div>
+      </Router>
       </div>
     );
   }
