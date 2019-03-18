e5bf8e8c71110ce78da9938233cfdb92056f4f20
Natalia Ortiz <natorgom@gmail.com>
2019-03-18 18:36:06 +0100

    Changes in App.js for socket.io - chat

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
+
+import openSocket from 'socket.io-client';
+
+import Chat from './components/chat/chat.js';
+
+const socket = openSocket('http://localhost:3001/');
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Chat socket={socket}></Chat>
     );
   }
 }
