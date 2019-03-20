3f6bc045f4d28684151417449690325104d3187c
Sue <sue.li@post.harvard.edu>
2019-03-20 20:49:46 +0100

    displaying additional tutor info

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -4,31 +4,24 @@ import './card.scss';
 class Card extends Component {
 
   render() {
-    if (this.props.chatNow) {
       return (
         <div className="card-container">
           <div className="card-avatar">
+          <div><img src={this.props.tutor.profileBadge} alt="profile-badge" width="50px"/></div>
+            <div>{this.props.tutor.firstname} {this.props.tutor.lastname}</div>
             <div>{this.props.tutor.username}</div>
-            <div>{this.props.tutor.credits}</div>
-            <div>Status</div>
           </div>
           <div className="card-body">
             <div>{this.props.offer.dateSubmitted}</div>
             <div>{this.props.offer.message}</div>
-            <div># Karma earned so far</div>
+            <div>{this.props.tutor.karma} Karma earned so far</div>
           </div>
           <div className="card-action">
           <button onClick={this.props.chatNow}>chat now</button>
           </div>
         </div>
       )
-    } else {
-      return (
-        <h1>All available questions</h1>
-      )
-    }
-
+    } 
   }
-}
 
 export default Card;
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -31,22 +31,23 @@ export const updateTutors = (tutor) => ({
 })
 
 
-
 export const fetchQuestionAndOffers = (questionid) => {
   return function (dispatch) {
     dispatch(requestOffers());
+
     // const token = localStorage.getItem('token');
+    // above - real token OR - use the token below
     const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEyMzQ1NjcsInVzZXJuYW1lIjoiTW9jayBMZWFybmVyIiwiZmlyc3ROYW1lIjoiRmlyc3QgTGVhcm5lciBOYW1lIiwibGFzdE5hbWUiOiJMYXN0IExlYXJuZXIgTmFtZSIsImVtYWlsIjoibW9ja2xlYXJuZXJAZ21haWwuY29tIiwiY3JlZGl0cyI6MCwia2FybWEiOjAsImF2YWlsYWJsZSI6bnVsbCwicHJvZmlsZUJhZGdlIjoiaHR0cHM6Ly9pbWFnZS5mbGF0aWNvbi5jb20vaWNvbnMvcG5nLzEyOC8yMzUvMjM1Mzk0LnBuZyIsImNyZWF0ZWRBdCI6IjIwMTktMDMtMjBUMTg6Mzg6MzcuOTM3WiIsInVwZGF0ZWRBdCI6IjIwMTktMDMtMjBUMTg6Mzg6MzcuOTM3WiIsImlhdCI6MTU1MzEwNzk1OSwiZXhwIjoxNTU1Njk5OTU5fQ.XCzJ17SrRP567urQDbEPRDtuireYM_kGo6hIxp3hDkY"
+    
     if (token) {
-      // return fetch(`http://localhost:4000/questions/${questionid}/offers`, {
-      return fetch(`${process.env.REACT_APP_END_POINT_URL}/questions/${questionid}/offers`, {
+      return fetch(`http://localhost:4000/questions/${questionid}/offers`, {
         method: 'GET',
         headers: {
           'Authorization': 'Bearer ' + token,
           'Accept': 'application/json'
         },
         })
-        .then(res => res.json()) // returns question and array of offers 
+        .then(res => res.json()) // res = question object and array of offer objects
         .then(res => {
           const offers = res.offers;
           Promise.all(offers.map(offer => fetch(`http://localhost:4000/users/${offer.tutor}`, {
@@ -57,16 +58,16 @@ export const fetchQuestionAndOffers = (questionid) => {
               },
               })
               .then(res => {
-                const tutor = res.json();
+                const tutor = res.json(); // res = array of tutor objects
                 return tutor;
               })
               .then(tutor => dispatch(updateTutors(tutor))
           )))
-          return res;
+          return res; // res = question object and array of offer objects
         })
         .then(res => {
-          dispatch(updateQuestion(res.question));
-          dispatch(updateOffers(res.offers))
+          dispatch(updateQuestion(res.question)); // dispatch question to store
+          dispatch(updateOffers(res.offers)) // dispatch offers to the store
         })
     } else {
       console.log('User needs to log in to see their questions');
