83e08796209cca40e54763bd7f59a6ae6ce65575
Natalia Ortiz <natorgom@gmail.com>
2019-03-22 12:05:38 +0100

    Revert "Answer page feature completed and integrated "

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -1,9 +1,7 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import reducers from './reducers';
-import thunk from 'redux-thunk';
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)),);
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
