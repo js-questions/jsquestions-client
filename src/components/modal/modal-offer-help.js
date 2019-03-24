7dad71d4cc471ee6e827d3bd6f1a9020f28d2a71
Amber-Williams <ambercwilliams91@gmail.com>
2019-03-24 20:55:28 +0100

    Changed modal component to become a function component in order to use React-Springfor animations

--- src/components/modal/modal-offer-help.js
+++ src/components/modal/modal-offer-help.js
@@ -1,36 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component, useState  } from 'react';
 import '../log-in/log-in.scss';
 import { Link } from "react-router-dom";
+import { useSpring, animated, config } from 'react-spring';
 
-class ModalOfferHelp extends Component {
-  state = {
+
+const offerHelp = (e, modalInfo, props) => {
+  e.preventDefault();
+  props.sendOffer(modalInfo)
+  props.closeOfferModal()
+}
+
+function ModalOfferHelp(props) {
+  const [modalInfo, setModalInfo ] = React.useState({
     message: null,
-    questionid: this.props.modalRef.questionid,
+    questionid: props.modalRef.questionid,
     expiration: Date.now() + 30
-  }
+  })
+  
+  const props2 = useSpring({ to: {opacity: 1, width: '600px', height: '600px'}, from: {opacity: 0, width: '0px', height: '0px'}, config: {duration:500}})
+  const props3 = useSpring({ opacity: 1, from: {opacity: 0}, config: {duration:2000}})
+  const props4 = useSpring({ opacity: 1, from: {opacity: 0}, config: {duration:3000}})
 
-  offerHelp = (e) => {
-    e.preventDefault();
-    this.props.sendOffer(this.state)
-    this.props.closeOfferModal()
-  }
-
-  render() {
-    return (
-      <div className="backdrop">
-        <div className="modal">
-          <button onClick={()=>this.props.closeOfferModal()}>X</button>
-          <div>{this.props.modalRef.title}</div>
-          <div>{this.props.modalRef.description}</div>
-          <form>
-            <input type='text' placeholder='Explain how you can help' onChange={(event) => this.setState({message: event.target.value})} />
-            <input type='text' value='30'/>
-            <button onClick={(e)=> this.offerHelp(e)}>{this.props.modalRef.button}</button>
-          </form>
-        </div>
+  return (
+    <div className="backdrop">
+      <animated.div style={props2}>
+      <div className="modal">
+        <animated.div style={props3}>
+        <button onClick={()=>props.closeOfferModal()}>X</button>
+        <div>{props.modalRef.title}</div>
+        <div>{props.modalRef.description}</div>
+        <form>
+          <input type='text' placeholder='Explain how you can help' onChange={(event) => setModalInfo({message: event.target.value, questionid: props.modalRef.questionid, expiration: Date.now() + 30})} />
+          <input type='text' value='30'/>
+          <animated.div style={props4}>
+          <button onClick={(e)=> offerHelp(e, modalInfo, props)}>{props.modalRef.button}</button>
+          </animated.div>
+        </form>
+        </animated.div>
       </div>
-    )
-  }
+      </animated.div>
+    </div>
+  )
 }
 
 export default ModalOfferHelp;
