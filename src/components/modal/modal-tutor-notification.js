6a99c28a979549410dad5c17f7114daed20d45d0
Natalia Ortiz <natorgom@gmail.com>
2019-03-27 11:39:16 +0100

    Add incoming call information
    Add incoming call information:
    - username calling
    - question title

--- src/components/modal/modal-tutor-notification.js
+++ src/components/modal/modal-tutor-notification.js
@@ -6,12 +6,11 @@ import '../../phone.scss';
 class TutorInvitation extends Component {
 
   render() {
-    console.log('Tutor invitation modal is receiving', this.props)
-    // We need to add the learner and question information to this modal. Styling.
     return (
       <div className="backdrop">
         <div className="modal">
-          <h2>Incoming call!</h2>
+          <h2 className="modal__incoming-call">Incoming call from {this.props.learner.username}!</h2>
+          <p className="modal__incoming-call">{this.props.question.title}</p>
           <Link className="link-style" onClick={this.props.enterChatroom} to={`/chat/${this.props.question.room_id}/${this.props.question.question_id}/tutor`}>
             <i className="Phone is-animating"></i>
           </Link>
