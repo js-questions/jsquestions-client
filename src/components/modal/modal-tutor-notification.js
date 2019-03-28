2f743425ccf5c25afa516ec06ca6947da7af0b13
Amber-Williams <ambercwilliams91@gmail.com>
2019-03-28 10:51:35 +0100

    Tutor notification component now has modal animation

--- src/components/modal/modal-tutor-notification.js
+++ src/components/modal/modal-tutor-notification.js
@@ -2,22 +2,30 @@ import React, { Component } from 'react';
 import '../log-in/log-in.scss';
 import { Link } from "react-router-dom";
 import '../../phone.scss';
+import { useSpring, animated } from 'react-spring';
 
-class TutorNotification extends Component {
+function TutorNotification (props) {
 
-  render() {
-    return (
-      <div className="backdrop">
-        <div className="modal">
-          <h2 className="modal__incoming-call">Incoming call from {this.props.learner.username}!</h2>
-          <p className="modal__incoming-call">{this.props.question.title}</p>
-          <Link className="link-style" onClick={this.props.enterChatroom} to={`/chat/${this.props.question.room_id}/${this.props.question.question_id}/tutor`}>
+  const showHide = useSpring({ reverse: !props.question, from: {display: 'none'}, to: {display: 'flex'}, delay: (_) => !props.question ? 500 : 0 })
+  const modal = useSpring({ reverse: !props.question, to: {opacity: 1, top: '30%'}, from: {opacity: 0, top: '-46%', right: '25%', left: '25%'}, config: {duration: !props.question ? 500 : 500}})
+
+  console.log(props)
+  return (
+    <div>
+    <animated.div style={showHide} className="backdrop" >
+    </animated.div>
+      <animated.div style={showHide} >
+        <animated.div className="modal" style={modal}>
+          <h2 className="modal__incoming-call">Incoming call from {props.learner.username}!</h2>
+          <p className="modal__incoming-call">{props.question.title}</p>
+          <Link className="link-style" onClick={props.enterChatroom} to={`/chat/${props.question.room_id}/${props.question.question_id}/tutor`}>
             <i className="Phone is-animating"></i>
           </Link>
-        </div>
-      </div>
-    )
-  }
+        </animated.div>
+      </animated.div>
+    </div>
+
+  )
 }
 
 export default TutorNotification;
