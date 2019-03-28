d7772ff331b791188c4e16391422898ee487fdff
Amber-Williams <ambercwilliams91@gmail.com>
2019-03-28 10:56:36 +0100

    Cleaned up code

--- src/components/modal/modal-tutor-notification.js
+++ src/components/modal/modal-tutor-notification.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../log-in/log-in.scss';
 import { Link } from "react-router-dom";
 import '../../phone.scss';
@@ -9,7 +9,6 @@ function TutorNotification (props) {
   const showHide = useSpring({ reverse: !props.question, from: {display: 'none'}, to: {display: 'flex'}, delay: (_) => !props.question ? 500 : 0 })
   const modal = useSpring({ reverse: !props.question, to: {opacity: 1, top: '30%'}, from: {opacity: 0, top: '-46%', right: '25%', left: '25%'}, config: {duration: !props.question ? 500 : 500}})
 
-  console.log(props)
   return (
     <div>
     <animated.div style={showHide} className="backdrop" >
