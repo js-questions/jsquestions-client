a3fc2c633a2d71dd374ac0b54a69993d7a35677e
Amber-Williams <ambercwilliams91@gmail.com>
2019-03-28 15:38:52 +0100

    more edits

--- src/components/modal/modal-tutor-notification.js
+++ src/components/modal/modal-tutor-notification.js
@@ -7,7 +7,8 @@ import { useSpring, animated } from 'react-spring';
 function TutorNotification (props) {
   var scrollY = window.pageYOffset;
   const showHide = useSpring({ reverse: !props.question, from: {display: 'none'}, to: {display: 'flex'}, delay: (_) => !props.question ? 500 : 0 })
-  const modal = useSpring({ reverse: !props.showModal, to: {opacity: 1, top: `${scrollY + 200}px `}, from: {opacity: 0, top: '-1000px', right: '25%', left: '25%'}, config: {duration: !props.showModal ? 500 : 500}})
+  const modal = useSpring({ reverse: !props.question, to: {opacity: 1, top: `${scrollY + 200}px `}, from: {opacity: 0, top: '-1000px', right: '25%', left: '25%'}, config: {duration: !props.showModal ? 500 : 500}})
+  
   return (
     <div>
     <animated.div style={showHide} className="backdrop" >
